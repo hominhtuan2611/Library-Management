6b040fad80e55dc937d4abb2b3f24f67d19f866d
Tuan <hominhtuan2611@gmail.com>
2019-09-18 07:34:47 +0700

    Added tables Customers, Books, Personnel.

---
+++ LibraryManagement.Database/Table/Books.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Books]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Author] NVARCHAR(30) NOT NULL, 
+    [Publisher] NVARCHAR(30) NULL, 
+    [PublishingYear] INT NULL, 
+    [TotalPage] INT NULL, 
+    [Summary] INT NULL, 
+    [BookType] INT NULL, 
+    [Status] BIT NOT NULL DEFAULT 1, 
+    [CreatedDay] DATE NULL, 
+    [CreatedBy] INT NULL, 
+    [ModifiedDay] DATE NULL, 
+    [ModifiedBy] INT NULL
+)
---
+++ LibraryManagement.Database/Table/Pesonnels.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Pesonnels]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(30) NOT NULL, 
+    [Gender] NVARCHAR(10) NOT NULL, 
+    [Birthday] DATE NOT NULL, 
+    [IdCard] NCHAR(12) NOT NULL, 
+    [Address] NVARCHAR(50) NULL, 
+    [Phone] NCHAR(11) NULL, 
+	[Position] NVARCHAR(30) NULL,
+    [Enabe] BIT NOT NULL DEFAULT 1, 
+    [CreatedDay] DATE NULL, 
+    [CreatedBy] INT NULL, 
+    [ModifiedDay] DATE NULL, 
+    [ModifiedBy] INT NULL
+)
