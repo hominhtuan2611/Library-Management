a372bb8b6c1010bf96b4e0159b9a0e83dcb17342
homan13 <homan13@HOMAN>
2019-11-10 10:02:30 +0700

    templateClient

---
+++ LibraryManagement.Web/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Product1()
+        {
+            return View();
+        }
+
+        public IActionResult Product2()
+        {
+            return View();
+        }
+    }
+}
