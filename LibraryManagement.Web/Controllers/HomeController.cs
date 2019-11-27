93f618f36d5e16896b6b2d43579ca76c5d6609a0
Sang Nguyen <nguyennhus293@gmail.com>
2019-11-28 00:58:01 +0700

    Changed to CTPhieuMuon & Sach Admin controller
    Modifed the client UI

--- LibraryManagement.Web/Controllers/HomeController.cs
+++ LibraryManagement.Web/Controllers/HomeController.cs
@@ -21,40 +21,41 @@ namespace LibraryManagement.Web.Controllers
         private readonly LibraryDBContext _context;
 
         public IConfiguration _configuration;
-        private readonly IHostingEnvironment _appEnvironment;
 
         private HttpClient _apiService;
         private readonly string apiAddress;
 
-        public HomeController(LibraryDBContext context, IConfiguration configuration, IHostingEnvironment appEnvironment)
+        public HomeController(LibraryDBContext context, IConfiguration configuration)
         {
             _context = context;
             _configuration = configuration;
-            _appEnvironment = appEnvironment;
 
             apiAddress = _configuration.GetSection("ApiAddress").GetSection("Url").Value;
             _apiService = ApiService.GetAPI(apiAddress);
         }
+
         public async Task<IActionResult> Index()
         {
-            var loaisach = await _apiService.GetAsync("api/Loaisach").Result.Content.ReadAsAsync<List<LoaiSach>>();
-            var list_sach = new List<Sach>();
-            var sach = new Sach();
-            var tuple = new Tuple<List<LibraryManagement.API.Models.LoaiSach>, List<LibraryManagement.API.Models.Sach>, LibraryManagement.API.Models.Sach>(loaisach, list_sach, sach);
-            return View(tuple);
-        }
-        public async Task<IActionResult> Login()
-        {
-            var loaisach = await _apiService.GetAsync("api/Loaisach").Result.Content.ReadAsAsync<List<LoaiSach>>();
+            var loaisach = await _apiService.GetAsync("api/loaisach").Result.Content.ReadAsAsync<List<LoaiSach>>();
             var list_sach = new List<Sach>();
             var sach = new Sach();
             var tuple = new Tuple<List<LibraryManagement.API.Models.LoaiSach>, List<LibraryManagement.API.Models.Sach>, LibraryManagement.API.Models.Sach>(loaisach, list_sach, sach);
             return View(tuple);
         }
 
-        public async void CheckLogin(string name, string pass)
+        public async Task<IActionResult> Login()
         {
-            HttpContext.Session.SetString(CommonConstants.Docgia_Session,"");
+            var loaisach = await _apiService.GetAsync("api/loaisach").Result.Content.ReadAsAsync<List<LoaiSach>>();
+            var list_sach = new List<Sach>();
+            var sach = new Sach();
+            var tuple = new Tuple<List<LibraryManagement.API.Models.LoaiSach>, List<LibraryManagement.API.Models.Sach>, LibraryManagement.API.Models.Sach>(loaisach, list_sach, sach);
+            return View(tuple);
+        }
+
+        [HttpPost]
+        public void CheckLogin(string name, string pass)
+        {
+            HttpContext.Session.SetString(CommonConstants.Docgia_Session, "");
             string password = Password_Encryptor.HashSHA1(pass);
             var docGia = _context.DocGia.Where(m => m.Username == name && m.Password == pass).FirstOrDefault();
             if (docGia == null)
@@ -64,11 +65,12 @@ namespace LibraryManagement.Web.Controllers
             else
             {
                 HttpContext.Session.SetObject<int>(CommonConstants.Docgia_Session, docGia.Id);
-                Response.Redirect("index");
+                Response.Redirect("Index");
             }
             /*return Index();*/
             //return View();
         }
+
         public IActionResult Privacy()
         {
             return View();
