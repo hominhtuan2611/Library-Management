f369905b582e7c36e43bb68f4549c3a6d07d6b67
homan <hothiman.sgu@gmail.com>
2019-12-02 22:01:24 +0700

    1

--- LibraryManagement.Web/Controllers/SingleController.cs
+++ LibraryManagement.Web/Controllers/SingleController.cs
@@ -32,8 +32,7 @@ namespace LibraryManagement.Web.Controllers
         {
 
             var loaisach = await _apiService.GetAsync("api/LoaiSach").Result.Content.ReadAsAsync<List<LoaiSach>>();
-            var list_sach = new List<Sach>();
-            HttpContext.Session.SetObject("dssach", list_sach); 
+            var list_sach = new List<Sach>(); 
             var sach = await _apiService.GetAsync($"api/sach/{id}").Result.Content.ReadAsAsync<Sach>();
             var tuple = new Tuple<List<LibraryManagement.API.Models.LoaiSach>, List<LibraryManagement.API.Models.Sach>, LibraryManagement.API.Models.Sach>(loaisach, list_sach, sach);
             return View(tuple);
